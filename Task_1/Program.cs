4c9e0d4d5834b9cba4df610e7c6bcf85da922f7a
Ilya Yurchenkov <yurchenkov.99@gmail.com>
2022-09-07 19:37:36 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1
+Console.Clear();
+int n = ReadInt("Введите число n: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
+void PrintNumber(int n, int count)
+{
+  if (count > n) return;
+  PrintNumber(n, count + 1);
+  Console.Write(count + ", ");
+}
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
